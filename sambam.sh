7812be841379ea10606b12f826f01b5dcc154e90
Brendan Lahm <brendanlahm@gmail.com>
2020-05-15 02:21:26 +0200

    sambam

---
+++ sambam.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+#  example submit script for a serial job
+#  submit by  sbatch serial-job.sh
+#
+#  specify the job name
+#SBATCH --job-name=bam
+#  how many cpus are requested
+#SBATCH --ntasks=4
+#  run on one node, important if you have more than 1 ntasks
+#SBATCH --nodes=1
+#  maximum walltime
+#SBATCH --time=100:00:00
+#  maximum requested memory
+#SBATCH --mem=50G
+#  write std out and std error to these files
+#SBATCH --error=essai_aln_280.sterr
+#SBATCH --output=essai_aln_280.stout
+#  send a mail for job start, end, fail, etc.
+#SBATCH --mail-type=NONE
+#SBATCH --mail-user=lahm@evolbio.mpg.de
+#  which partition?
+#  there are global,testing,highmem,standard,fast
+#SBATCH --partition=global
+
+
+for each in *.sam
+do
+samtools view -bS ${each} > ${each%.sam}.bam
+done
