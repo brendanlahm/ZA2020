3b0c8ff5446497b383b8d9ab9e23d68fd133aa7e
Brendan Lahm <brendanlahm@gmail.com>
2020-10-15 11:41:58 +0200

    Contig selection correction

--- circos/contig_select14_Za100.sh
+++ circos/contig_select14_Za100.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 
-awk '{ if ((NR>1)&&($0~/^>/)) { printf("\n%s", $0); } else if (NR==1) { printf("%s", $0); } else { printf("\t%s", $0); } }' /home/lahm/Desktop/assemblies/Za100_canu.unitigs.fasta | grep -Ff contigs_Za100_14.txt - | tr "\t" "\n" > Za100_Zt469/Za100_14.fa
+awk '{ if ((NR>1)&&($0~/^>/)) { printf("\n%s", $0); } else if (NR==1) { printf("%s", $0); } else { printf("\t%s", $0); } }' /home/lahm/Desktop/Assemblies/Za100_canu.unitigs.fasta | grep -Ff contigs_Za100_14.txt - | tr "\t" "\n" > Za100_Zt469/Za100_14.fa
